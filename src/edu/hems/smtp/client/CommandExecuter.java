dfdf6c15ad45a7002d45e2bb3e0914b02f6a473f
Hemant Saxena <java.hemant@gmail.com>
2018-03-25 14:38:51 -0400

    basic components are structured. Working good.

--- src/edu/hems/smtp/client/CommandExecuter.java
+++ src/edu/hems/smtp/client/CommandExecuter.java
@@ -1,9 +1,77 @@
 package edu.hems.smtp.client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class CommandExecuter {
 	
-	public CommandExecuter(String hostName) {
-		// TODO Auto-generated constructor stub
+	Socket emailSocket;
+	BufferedReader br;
+	OutputStream os;
+	
+	public String init(String hostName, int port) throws UnknownHostException, IOException {
+		emailSocket = new Socket(hostName, port);
+		
+		InputStream is = emailSocket.getInputStream();
+		br = new BufferedReader(new InputStreamReader(is));
+
+		// Get a reference to the socket's output stream.
+		os = emailSocket.getOutputStream();
+		return readResponse();
+	}
+	
+	public String executeCommand(Command cmd) throws UnsupportedEncodingException, IOException {
+		String response = null;
+		
+		String cmdStr = cmd.getCommand();
+		cmdStr+="\r\n";
+		
+		System.out.println("Command: " + cmdStr);		
+		writeRequest(cmdStr);
+		
+		response = readResponse();
+		System.out.println("Cmd Response: " + response);
+		
+		return response;
+	}
+	
+	public String executeMessage(String message) throws IOException {
+		String response;
+		
+		String smtpMessage = "";
+		
+		smtpMessage += "from: hsaxena@radar.gsw.edu \r\n";
+		smtpMessage += "to: hemantsaxena@yahoo.com \r\n";
+		smtpMessage += "subject: smtp lab message \r\n";
+		smtpMessage += message;
+		
+		smtpMessage += "\r\n.\r\n";
+		writeRequest(smtpMessage);
+		
+		response = readResponse();
+		System.out.println("Message Response: " + response);
+		
+		return response;
+	}
+	
+	private void writeRequest(String req) throws UnsupportedEncodingException, IOException {
+		os.write(req.getBytes("US-ASCII"));
+	}
+	
+	private String readResponse() throws IOException {
+		return br.readLine();
+	}
+	
+	public void close() throws IOException {
+		os.close();
+		br.close();
+		emailSocket.close();
 	}
 
 }
--- src/edu/hems/smtp/client/SMTPClient.java
+++ src/edu/hems/smtp/client/SMTPClient.java
@@ -7,106 +7,53 @@ import java.util.*;
 public class SMTPClient {
 
 	public static void main(String[] args) throws Exception {
-		// TODO code application logic here
-		// Establish a TCP connection with the mail server.
-		//System.out.println("Enter the mail server you wish to connect to (example:  edge.nunet.nova.edu):\n");
-		String hostName = new String();
-		//Scanner emailScanner = new Scanner(System.in);smtp.gswcm.net
-		hostName = "smtp.gswcm.net"; //emailScanner.next();
-		Socket emailSocket = new Socket(hostName, 25);
-		// Create a BufferedReader to read a line at a time.
-		InputStream is = emailSocket.getInputStream();
-		InputStreamReader isr = new InputStreamReader(is);
-		BufferedReader br = new BufferedReader(isr);
-		// Read greeting from the server.
-		String response = br.readLine();
-		System.out.println("Response 1: " + response);
-		if (!response.startsWith("220")) {
+		CommandExecuter cmdExecutor = new CommandExecuter();
+		
+		String response1 = cmdExecutor.init("smtp.gswcm.net", 25);
+		if (!response1.startsWith("220")) {
 			throw new Exception("220 reply not received from server.\n");
 		}
-		// Get a reference to the socket's output stream.
-		OutputStream os = emailSocket.getOutputStream();
-		// Send HELO command and get server response.
-		//System.out.println("Enter the name of your mail domain (example:  hotmail.com):");
-		//String heloDomain = emailScanner.next();
-		String fullHeloCommand = "HELO " + "pg-01" + "\r\n";
-		System.out.print("Command: " + fullHeloCommand);
-		os.write(fullHeloCommand.getBytes("US-ASCII"));
-		response = br.readLine();
-		System.out.println("Response 2: " + response);
-		if (!response.startsWith("250")) {
-			throw new Exception("250 reply not received from server.\n");
-		}
-		// Send MAIL FROM command.
-		//System.out.println("Please enter your (source) e-mail address (example: me@myexample.com:\n");
-		//String sourceAddress = emailScanner.next();
-		String mailFromCommand = "MAIL From: " + "hsaxena@radar.gsw.edu" + "\r\n";
-		System.out.println("Command: " + mailFromCommand);
-		os.write(mailFromCommand.getBytes("US-ASCII"));
-		response = br.readLine();
-		System.out.println("Response 3: " + response);
-		if (!response.startsWith("250"))
-			throw new Exception("250 reply not received from server.\n");
-
-		String mailFromCommand2 = "SUBJECT: " + "This is for smtp assignment" + "\r\n";
-		System.out.println("Command: " + mailFromCommand2);
-		os.write(mailFromCommand2.getBytes("US-ASCII"));
-		response = br.readLine();
-		System.out.println("Response 3.1: " + response);
-		if (!response.startsWith("250"))
-			throw new Exception("250 reply not received from server.\n");
 		
-		// Send RCPT TO command.
-		//System.out.println("Please type the destination e-mail address (example:  example@nova.edu):\n");
-		String destEmailAddress = new String();
-		//destEmailAddress = emailScanner.next();
-		String fullAddress = new String();
-		fullAddress = "RCPT TO: " + "java.hemant@gmail.com" + "\r\n";
-		System.out.println("Command: "+ fullAddress);
-		os.write(fullAddress.getBytes("US-ASCII"));
-		response = br.readLine();
-		System.out.println("Response 4: " + response);
-		if (!response.startsWith("250")) {
+		String response2 = cmdExecutor.executeCommand(new Command("HELO pg-01"));
+		if (!response2.startsWith("250")) {
 			throw new Exception("250 reply not received from server.\n");
 		}
-		// Send DATA command.
-		String dataString = new String();
-		dataString = "DATA\r\n";
-		System.out.println("Command: " + dataString);
-		os.write(dataString.getBytes("US-ASCII"));
-		response = br.readLine();
-		if (!response.startsWith("354"))
+		
+		String response3 = cmdExecutor.executeCommand(new Command("MAIL From: " + "hsaxena@radar.gsw.edu"));
+		if (!response3.startsWith("250")) {
+			throw new Exception("250 reply not received from server.\n");
+		}
+		
+		String response4 = cmdExecutor.executeCommand(new Command("RCPT TO: " + "hemantsaxena@yahoo.com"));
+		if (!response4.startsWith("250")) {
+			throw new Exception("250 reply not received from server.\n");
+		}
+		
+		String response5= cmdExecutor.executeCommand(new Command("DATA"));
+		if (!response5.startsWith("354")) {
 			throw new Exception("354 reply not received from server.\n");
-		System.out.println("Response 5: " + response);
-		// Send message data.
-		//System.out.println("Enter your message, enter '.' on a separate line to end message data entry:\n");
-		String input = "\r\n from: hsaxena@radar.gsw.edu\r\n" + 
-				"to: java.hemant@gmail.com\r\n" + 
-				"subject: This is the President\r\n" + 
+		}
+		
+
+		String message = "Hello, Student\r\n" + 
 				"\r\n" + 
-				"Hello, Student\r\n" + 
+				"This is test message from lab. Please ignore.\r\n" + 
+				"\r\n" + 
 				"\r\n" + 
 				"--\r\n" + 
-				"President\r\n" + 
-				"\r\n.\r\n";
-//		while (input.charAt(0) != '.') {
-//			input = emailScanner.next();
-//			os.write(input.getBytes("US-ASCII"));
-//		}
-		// End with line with a single period.
-		os.write(input.getBytes("US-ASCII"));
-		response = br.readLine();
-		System.out.println("Response 6: " + response);
-		if (!response.startsWith("250"))
-			throw new Exception("250 reply not received from server\n");
+				"Hemant\r\n";
+		String response6= cmdExecutor.executeMessage(message);
+		if (!response6.startsWith("250")) {
+			throw new Exception("250 reply not received from server.\n");
+		}
+		
+		
+		String response7 = cmdExecutor.executeCommand(new Command("QUIT"));
+		if (!response7.startsWith("221")) {
+			throw new Exception("2221 reply not received from server.\n");
+		}
 
-		// Send QUIT command.
-		String quitCommand = new String();
-		quitCommand = "QUIT\r\n";
-		System.out.println("Command: " + quitCommand);
-		os.write(quitCommand.getBytes("US-ASCII"));
-		response = br.readLine();
-		System.out.println("Response 7: " + response);
-		emailSocket.close();
+		cmdExecutor.close();
 	}
+	
 }
